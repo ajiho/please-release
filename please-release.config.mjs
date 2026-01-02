9d0f1ebd0e2d991ea03868f1da463c1debae08a3
ajiho <lujiahao@88.com>
2026-01-02 18:17:03 +0800

    789
    Signed-off-by: ajiho <lujiahao@88.com>

--- please-release.config.mjs
+++ please-release.config.mjs
@@ -18,9 +18,12 @@ export default {
       logger.info(`after selecting version:${version}`);
     },
 
-    "after:bump": ({ version, cancel, logger }) => {
-      logger.info(`Updated version to:${version}`);
-    },
+    "after:bump": [
+      "npm run changelog",
+      async () => {
+        console.log("after:bump");
+      },
+    ],
 
     "after:release": "echo 已推送 v${version} ",
   },
