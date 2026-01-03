2ca764a2d3f1390652e224ad47eb3311107e9214
ajiho <lujiahao@88.com>
2026-01-04 00:10:35 +0800

    fds
    Signed-off-by: ajiho <lujiahao@88.com>

--- please-release.config.mjs
+++ please-release.config.mjs
@@ -8,23 +8,19 @@ export default {
   },
 
   hooks: {
-    "before:init": ["npm run test"],
-
-    "before:selectVersion": ({ logger }) => {
-      logger.info("Before selecting version");
-    },
-
-    "after:selectVersion": ({ version, logger }) => {
-      logger.info(`after selecting version:${version}`);
-    },
-
-    "after:bump": [
-      "npm run changelog",
-      async () => {
-        console.log("after:bump");
-      },
-    ],
-
-    "after:release": "echo 已推送 v${version} ",
+    // "before:init": ["npm run test"],
+    // "before:selectVersion": ({ logger }) => {
+    //   logger.info("Before selecting version");
+    // },
+    // "after:selectVersion": ({ version, logger }) => {
+    //   logger.info(`after selecting version:${version}`);
+    // },
+    // "after:bump": [
+    //   "npm run changelog",
+    //   async () => {
+    //     console.log("after:bump");
+    //   },
+    // ],
+    // "after:release": "echo 已推送 v${version} ",
   },
 };
