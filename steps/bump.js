0247796a5b6f36a6d6cfe665cebca8c9ea2de897
ajiho <lujiahao@88.com>
2026-01-04 00:18:02 +0800

    fds
    Signed-off-by: ajiho <lujiahao@88.com>

--- steps/bump.js
+++ steps/bump.js
@@ -1,9 +1,6 @@
 import { run } from "../utils/index.js";
 
 export async function bump(config, ctx) {
-  console.log("--------");
-  console.log(ctx.version);
-
   await run("npm", [
     "version",
     ctx.version,
