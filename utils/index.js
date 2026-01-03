5c407c49d9003b86e3c2d6b5887faa645432f8ca
ajiho <lujiahao@88.com>
2026-01-04 00:23:33 +0800

    dsa
    Signed-off-by: ajiho <lujiahao@88.com>

--- utils/index.js
+++ utils/index.js
@@ -12,10 +12,8 @@ export const logger = createConsola({
 });
 
 export async function getChangeset(cwd = process.cwd()) {
-  logger.log("\n");
   logger.log(`Changeset:`);
   await run("git", ["status", "--porcelain"], { stdio: "inherit", cwd });
-  logger.log("\n");
 }
 
 /**
