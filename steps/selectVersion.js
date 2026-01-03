2e388f614fbd17ba5d47a273a6f4ec7f021fec9e
ajiho <lujiahao@88.com>
2026-01-03 23:28:35 +0800

    dsa
    Signed-off-by: ajiho <lujiahao@88.com>

--- steps/selectVersion.js
+++ steps/selectVersion.js
@@ -74,7 +74,7 @@ export async function selectVersion(config, ctx) {
       name: "version",
       message: "输入或者选择自定义版本",
       choices: [
-        { title: "latest", value: "latest" },
+        { title: "0.0.28-alpha.0", value: "0.0.28-alpha.1" },
         { title: "next", value: "next" },
         { title: "beta", value: "beta" },
         { title: "alpha", value: "alpha" },
