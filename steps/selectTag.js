18db9f9c0c5405a33069bcf4380ea81cd60d9c1f
ajiho <lujiahao@88.com>
2026-01-04 12:44:57 +0800

    fds
    Signed-off-by: ajiho <lujiahao@88.com>

--- steps/selectTag.js
+++ steps/selectTag.js
@@ -1,29 +1,46 @@
 import prompts from "prompts";
+import semver from "semver";
 import { CancelledError } from "../errors.js";
 
 export async function selectTag(config, ctx) {
-  const tags = config.tags;
+  const isPrerelease = !!semver.prerelease(ctx.version);
+
+  const choices = config.tags.map((tag) => {
+    if (tag === "latest" && isPrerelease) {
+      return {
+        title: `${tag} (not allowed for prerelease)`,
+        value: tag,
+        disabled: true,
+      };
+    }
+
+    return {
+      title: tag,
+      value: tag,
+    };
+  });
 
   const { tag } = await prompts({
     type: "select",
     name: "tag",
     message: "Select tag type",
-    choices: tags,
+    choices,
   });
 
-  if (tag === undefined) {
+  if (!tag) {
     throw new CancelledError();
   }
 
-  const { yes: tagOk } = await prompts({
+  const { yes } = await prompts({
     type: "confirm",
     name: "yes",
-    message: `Releasing v${ctx.version} on ${tags[tag]}. Confirm?`,
+    message: `Releasing v${ctx.version} on ${tag}. Confirm?`,
     initial: true,
   });
 
-  if (!tagOk) {
+  if (!yes) {
     throw new CancelledError();
   }
-  ctx.tag = tags[tag];
+
+  ctx.tag = tag;
 }
